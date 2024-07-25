1c0fa11bcc4ea8834dee6bf88953b55b4b98d1f4
lakshyasuthar <lakshyasuthar.freelance@gmail.com>
2024-07-25 11:52:13 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import Home from "./pages/Main";
 import "../public/styles.css";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 function App() {
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+
   useEffect(() => {
-    const name = localStorage.getItem("userData").split('"');
-    const favicon = document.getElementById("favicon");
-    const avatar = localStorage.getItem("avatar").split('"');
-    document.title = `${name[1]}'s LinkTree`;
-    favicon.setAttribute("href", `${avatar[1]}`);
-  });
+    if (auth) {
+      const name = localStorage.getItem("userData")?.split('"');
+      const favicon = document.getElementById("favicon");
+      const avatar = localStorage.getItem("avatar")?.split('"');
+      document.title = `${name[1]}'s LinkTree`;
+      favicon.setAttribute("href", `${avatar[1]}`);
+    }
+  }, []);
   return (
     <>
       <Home />
