48eb65c315e6850cf8755ccdea4dd1f2d7454854
lakshyasuthar <lakshyasuthar.freelance@gmail.com>
2024-07-25 11:59:31 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import "../public/styles.css";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 function App() {
-  const auth = useSelector((state) => state.auth.isAuthenticated);
+  const auth = useSelector((state) => state?.auth?.isAuthenticated);
 
   useEffect(() => {
     if (auth) {
       const name = localStorage.getItem("userData")?.split('"');
       const favicon = document.getElementById("favicon");
       const avatar = localStorage.getItem("avatar")?.split('"');
-      document.title = `${name[1]}'s LinkTree`;
-      favicon.setAttribute("href", `${avatar[1]}`);
+      if (name && avatar && favicon) {
+        document.title = `${name[1]}'s LinkTree`;
+        favicon.setAttribute("href", `${avatar[1]}`);
+      }
     }
   }, []);
   return (
